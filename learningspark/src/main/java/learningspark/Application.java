4e1affc9913f31956abec2c31fee9fe03ea78b73
shashank-shark <shashankreddy231@gmail.com>
2019-10-03 19:00:19 +0530

    mysql conn - 5
    mysql conn - 5

--- learningspark/src/main/java/learningspark/Application.java
+++ learningspark/src/main/java/learningspark/Application.java
@@ -2,7 +2,11 @@ package learningspark;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
+import java.util.Properties;
+import static org.apache.spark.sql.functions.concat;
+import static org.apache.spark.sql.functions.lit;
 
 public class Application {
 	public static void main(String[] args) 
@@ -17,6 +21,21 @@ public class Application {
 		.option("header", true)
 		.load("src/main/resources/resources_and_comments");
 		
-		df.show();
+		// concat first and last column - Transformation
+        df = df.withColumn("full_name", concat(df.col("last_name"), lit(", "), df.col("first_name")));
+
+        // only the comments with bumbers
+        df = df.filter(df.col("comment").rlike("\\d+"))
+                .orderBy(df.col("last_name").asc());
+        // display the dataset
+        String dbConnectionUrl = "jdbc::postgresql://localhost/course_data";
+        Properties prop = new Properties();
+        prop.setProperty("driver", "org.postgresql.Driver");
+        prop.setProperty("user", "postgres");
+        prop.setProperty("password","password");
+
+        df.write()
+                .mode(SaveMode.Overwrite)
+                .jdbc(dbConnectionUrl, "project1", prop);
 	}
 }
