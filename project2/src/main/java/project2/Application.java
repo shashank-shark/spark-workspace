4796989e712e87b3be5e03a5cb5f78c5453a9a06
shashank-shark <shashankreddy231@gmail.com>
2019-10-04 16:12:53 +0530

    spark-project2
    spark-project2

---
+++ project2/src/main/java/project2/Application.java
@@ -0,0 +1,9 @@
+package project2;
+
+public class Application {
+	
+	public static void main (String[] args) {
+		InferCSVSchema parser = new InferCSVSchema();
+		parser.printSchema();
+	}
+}
---
+++ project2/src/main/java/project2/InferCSVSchema.java
@@ -0,0 +1,33 @@
+package project2;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class InferCSVSchema {
+	
+	public void printSchema () {
+		
+		SparkSession spark = new SparkSession.Builder()
+				.appName("Complex CSV to Dataset")
+				.master("local")
+				.getOrCreate();
+		
+		Dataset<Row> df = spark.read().format("csv")
+				.option("header", true)
+				.option("multiline", true)
+				.option("sep",";")
+				.option("quote", "^")
+				.option("dataframe", "M/d/y")
+				.option("inferSchema", true)
+				.load("src/main/resources/amazonProducts.txt");
+		
+		System.out.println("The dataframe content");
+		
+		df.show(7);
+		
+		System.out.println("Dataframe's Schema : ");
+		df.printSchema();
+	}
+
+}
